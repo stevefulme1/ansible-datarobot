3b16cf1880d7520bfff68d5826ce60726ca30f17
sfulmer <sfulmer@redhat.com>
2026-05-18 18:32:15 -0400

    fix: skip no-changelog rule, fix empty init, remove unused test imports

--- tests/unit/plugins/modules/test_model.py
+++ tests/unit/plugins/modules/test_model.py
@@ -5,7 +5,6 @@ __metaclass__ = type
 
 from unittest.mock import MagicMock, patch
 
-import pytest
 
 MODULE_PATH = "ansible_collections.stevefulme1.datarobot.plugins.modules.model"
 CLIENT_PATH = "ansible_collections.stevefulme1.datarobot.plugins.module_utils.api_client"
